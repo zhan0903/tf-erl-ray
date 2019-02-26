4cd3da25f31a58de6dec6fbc77310d15ba4bd00f
zhan0903 <z.han0903@qq.com>
2019-02-26 15:50:06 +1100

    debug

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ class ActorPolicy(object):
         self._s.run(self._train, feed_dict=batch_feed)
 
 
-@ray.remote(num_gpus=0.1)
+@ray.remote
 class Worker(object):
     def __init__(self, args):
         self.env = utils.NormalizedActions(gym.make(env_tag))
